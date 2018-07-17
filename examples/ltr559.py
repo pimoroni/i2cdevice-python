b7aed0caa6a1e719508d3516255a0014e44d4edc
Phil Howard <phil@gadgetoid.com>
2018-07-17 11:20:59 +0000

    Demonstrate use of with locking

--- examples/ltr559.py
+++ examples/ltr559.py
@@ -1,3 +1,4 @@
+import time
 import sys
 sys.path.insert(0, "../library/")
 from i2cdevice import MockSMBus, Device, Register, BitField
@@ -24,7 +25,7 @@ class Bit12Adapter(Adapter):
         return ((value & 0xFF00) >> 8) | ((value & 0x000F) << 8)
 
 
-ltr559 = Device(I2C_ADDR, i2c_dev=MockSMBus(1), bit_width=8, registers=(
+ltr559 = Device(I2C_ADDR, bit_width=8, registers=(
 
     Register('ALS_CONTROL', 0x80, fields=(
         BitField('gain', 0b00011100, adapter=LookupAdapter({1: 0b000, 2: 0b001, 4: 0b011, 8:0b011, 48:0b110, 96:0b111})),
@@ -94,8 +95,8 @@ ltr559 = Device(I2C_ADDR, i2c_dev=MockSMBus(1), bit_width=8, registers=(
     )),
 
     Register('PS_THRESHOLD', 0x90, fields=(
-        BitField('upper', 0xFF0F0000, adapter=Bit12Adapter()),
-        BitField('lower', 0x0000FF0F, adapter=Bit12Adapter())
+        BitField('upper', 0xFF0F0000, adapter=Bit12Adapter(), bit_width=16),
+        BitField('lower', 0x0000FF0F, adapter=Bit12Adapter(), bit_width=16)
     ), bit_width=32),
 
     # PS_OFFSET defines the measurement offset value to correct for proximity
@@ -108,8 +109,8 @@ ltr559 = Device(I2C_ADDR, i2c_dev=MockSMBus(1), bit_width=8, registers=(
     # An interrupt is triggered if values fall outside of this range.
     # See also INTERRUPT_PERSIST.
     Register('ALS_THRESHOLD', 0x97, fields=(
-        BitField('upper', 0xFFFF0000, adapter=U16ByteSwapAdapter()),
-        BitField('lower', 0x0000FFFF, adapter=U16ByteSwapAdapter())
+        BitField('upper', 0xFFFF0000, adapter=U16ByteSwapAdapter(), bit_width=16),
+        BitField('lower', 0x0000FFFF, adapter=U16ByteSwapAdapter(), bit_width=16)
     ), bit_width=32),
 
     # This register controls how many values must fall outside of the range defined
@@ -124,3 +125,132 @@ ltr559 = Device(I2C_ADDR, i2c_dev=MockSMBus(1), bit_width=8, registers=(
     ))
 
 ))
+
+
+if __name__ == "__main__":
+    with ltr559.PART_ID as PART_ID:
+        assert PART_ID.get_part_number() == 0x09
+        assert PART_ID.get_revision() == 0x02
+
+    print("""
+Found LTR-559.
+Part ID: 0x{:02x}
+Revision: 0x{:02x}
+    """.format(
+        ltr559.PART_ID.get_part_number(),
+        ltr559.PART_ID.get_revision())
+    )
+
+    print("""
+Soft Reset
+    """)
+    ltr559.ALS_CONTROL.set_sw_reset(1)
+    try:
+        while True:
+            status = ltr559.ALS_CONTROL.get_sw_reset()
+            print("Status: {}".format(status))
+            if status == 0:
+                break
+            time.sleep(1.0)
+    except KeyboardInterrupt:
+        pass
+
+
+    print("Setting ALS threshold")
+    # Modifying the fields of this register without a "with" statement will trigger
+    # two successive read/modify/write operations. Use "with" to optimise these out.
+    ltr559.ALS_THRESHOLD.set_lower(0x0001)
+    ltr559.ALS_THRESHOLD.set_upper(0xFFEE)
+    print("{:08x}".format(ltr559.values['ALS_THRESHOLD']))
+    with ltr559.ALS_THRESHOLD as ALS_THRESHOLD:
+        print("LOWER: ", ALS_THRESHOLD.get_lower())
+        assert ALS_THRESHOLD.get_lower() == 0x0001
+        assert ALS_THRESHOLD.get_upper() == 0xFFEE
+
+    print("Setting PS threshold")
+    ltr559.PS_THRESHOLD.set_lower(0)
+    ltr559.PS_THRESHOLD.set_upper(500)
+    print("{:08x}".format(ltr559.values['PS_THRESHOLD']))
+    with ltr559.PS_THRESHOLD as PS_THRESHOLD:
+        assert PS_THRESHOLD.get_lower() == 0
+        assert PS_THRESHOLD.get_upper() == 500
+
+    print("Setting integration time and repeat rate")
+    ltr559.PS_MEAS_RATE.set_rate_ms(100)
+    ltr559.ALS_MEAS_RATE.set_integration_time_ms(50)
+    ltr559.ALS_MEAS_RATE.set_repeat_rate_ms(50)
+    with ltr559.ALS_MEAS_RATE as ALS_MEAS_RATE:
+        assert ALS_MEAS_RATE.get_integration_time_ms() == 50
+        assert ALS_MEAS_RATE.get_repeat_rate_ms() == 50
+
+    print("""
+Activating sensor
+    """)
+
+    ltr559.INTERRUPT.set_mode('als+ps')
+    ltr559.PS_CONTROL.set_active(True)
+    ltr559.PS_CONTROL.set_saturation_indicator_enable(1)
+
+    with ltr559.PS_LED as PS_LED:
+        PS_LED.set_current_ma(50)
+        PS_LED.set_duty_cycle(1.0)
+        PS_LED.set_pulse_freq_khz(30)
+        PS_LED.write() # *MUST* be called to write the value when in context mode
+
+    ltr559.PS_N_PULSES.set_count(1)
+
+    with ltr559.ALS_CONTROL as ALS_CONTROL:
+        ALS_CONTROL.set_mode(1)
+        ALS_CONTROL.set_gain(4)
+        ALS_CONTROL.write()
+
+    with ltr559.ALS_CONTROL as ALS_CONTROL:
+        assert ALS_CONTROL.get_mode() == 1
+        assert ALS_CONTROL.get_gain() == 4
+
+    ltr559.PS_OFFSET.set_offset(69)
+
+    als0 = 0
+    als1 = 0
+    ps0 = 0
+    lux = 0
+
+    ch0_c = (17743, 42785, 5926, 0)
+    ch1_c = (-11059, 19548, -1185, 0)
+
+    try:
+        while True:
+            # By default any read from a register field will trigger a read from hardware
+            # and any write to a register field will trigger a write to hardware.
+            # Using the "with" statement overrides this behavior by locking the register
+            # value during the with context so that its value is only read once on context entry.
+            with ltr559.ALS_PS_STATUS as ALS_PS_STATUS:
+                ps_int = ALS_PS_STATUS.get_ps_interrupt() or ALS_PS_STATUS.get_ps_data()
+                als_int = ALS_PS_STATUS.get_als_interrupt() or ALS_PS_STATUS.get_als_data()
+
+            if ps_int:
+                ps0 = ltr559.PS_DATA.get_ch0()
+
+            if als_int:
+                with ltr559.ALS_DATA as ALS_DATA:
+                    als0 = ALS_DATA.get_ch0()
+                    als1 = ALS_DATA.get_ch1()
+
+                ratio = 1000
+                if als0 + als0 > 0:
+                    ratio = (als0 * 1000) / (als1 + als0)
+        
+                ch_idx = 3
+                if ratio < 450:
+                    ch_idx = 0
+                elif ratio < 640:
+                    ch_idx = 1
+                elif ratio < 850:
+                    ch_idx = 2
+                
+                lux = ((als0 * ch0_c[ch_idx]) - (als1 * ch1_c[ch_idx])) / 10000
+
+            print("Lux: {:06.2f}, Light CH0: {:04d}, Light CH1: {:04d}, Proximity: {:04d}  New Data LP: 0b{:01d}{:01d}".format(lux, als0, als1, ps0, als_int, ps_int))
+            time.sleep(0.05)
+    except KeyboardInterrupt:
+        pass
